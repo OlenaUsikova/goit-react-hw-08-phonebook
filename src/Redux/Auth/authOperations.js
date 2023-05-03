815eb4deea930fb7dd191a4a475f8ea1b03825ac
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-05-03 10:39:50 +0200

    2

---
+++ src/Redux/Auth/authOperations.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com//'
+
+const setToken = token => {
+	axios.defaults.headers.common.Authorization = `Bearer ${token}`
+}
+const clearToken = () => {
+	axios.defaults.headers.common.Authorization = ``
+}
+
+export const registrationThunk = createAsyncThunk(
+	'@@auth/registration',
+	async (credentials, thunkAPI) => {
+		try {
+			// console.log(credentials)
+			const res = await axios.post('users/signup', credentials)
+			setToken(res.data.token)
+			return res.data
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error)
+		}
+	}
+)
+export const loginThunk = createAsyncThunk(
+	'@@auth/login',
+	async (credentials, thunkAPI) => {
+		try {
+			const res = await axios.post('users/login', credentials)
+			setToken(res.data.token)
+			console.log(res)
+			return res.data
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error)
+		}
+	}
+)
+
+export const logoutThunk = createAsyncThunk(
+	'@@auth/logout',
+	async (_, thunkAPI) => {
+		try {
+			await axios.post('users/logout')
+			clearToken()
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error)
+		}
+	}
+)
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,40 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from 'Redux/contactsSlice';
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-//   whitelist: ['contacts'],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: persistedReducer,
-//   },
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
 export const store = configureStore({
   reducer: contactsReducer },
 );
-
-// https://644bb18e4bdbc0cc3a97f239.mockapi.io/contacts
